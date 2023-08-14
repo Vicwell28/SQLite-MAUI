1b789ac1313af3614185354ffa3e780864dfe1d0
Basurto, Victor {LALA} <victor.basurto@grupolala.com>
2023-08-13 23:41:18 -0600

    Add project files.

---
+++ SQLite-MAUI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SQLite_MAUI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SQLite-MAUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace SQLite_MAUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
